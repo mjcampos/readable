8a2949b2e3e1e9c4d24a1e4abebb0080215d4632
Michael Campos <camposmj89@gmail.com>
2017-10-22 23:35:42 -0700

    Added styles css

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,16 @@
 import {connect} from 'react-redux';
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import '../styles/styles.css';
+import {importPostsByCategory} from '../actions/posts';
 
 class Category extends Component {
+	componentWillMount() {
+		var {category} = this.props.match.params;
+
+		this.props.importPostsByCategory(category);
+	}
+
 	convertTimestampToDate(timestamp) {
 		var d = new Date(timestamp);
 
@@ -13,17 +21,15 @@ class Category extends Component {
 		var {category} = this.props.match.params;
 
 		return (
-			<div>
+			<div className="container">
 				<h2 className="text-center">Category: {category}</h2>
 
 				<ul>
 					{this.props.posts.map(post => (
-						<li key={post.id}>
-							<Link to=""><h3>{post.title}</h3></Link>
-							<p><b>Date:</b> {this.convertTimestampToDate(post.timestamp)}</p>
+						<li key={post.id} className="text-center">
+							<Link to={`${category}/${post.id}`}><h3>{post.title}</h3></Link>
 							<p><b>Author</b>: {post.author}</p>
-							<h4>{post.body}</h4>
-							<p>Vote Score: {post.voteScore}</p>
+							<p><b>Date:</b> {this.convertTimestampToDate(post.timestamp)}</p>
 						</li>
 					))}
 				</ul>
@@ -32,12 +38,10 @@ class Category extends Component {
 	}
 }
 
-function mapStateToProps(state, ownProps) {
-	var {category} = ownProps.match.params;
-
+function mapStateToProps(state) {
 	return {
-		posts: state.posts.filter(post => post.category === category)
+		posts: state.posts
 	};
 }
 
-export default connect(mapStateToProps, null)(Category);
+export default connect(mapStateToProps, {importPostsByCategory})(Category);
