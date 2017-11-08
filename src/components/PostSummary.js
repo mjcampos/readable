94053c58530b3d510f0259d19091bc9405c5292d
Michael Campos <camposmj89@gmail.com>
2017-11-07 23:04:19 -0800

    Added voting, deletion, and edit features are now included in Post Summary that can be viewed in both Main and Category pages

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {postVote, deletePost} from '../actions/posts';
 
 class PostSummary extends Component {
 	convertTimestampToDate(timestamp) {
@@ -9,16 +10,41 @@ class PostSummary extends Component {
 		return d.toDateString();
 	}
 
+	onVoteClick = (option) => {
+		var {id} = this.props.post;
+
+		this.props.postVote(id, {option});
+	}
+
 	render() {
 		var {post, comments} = this.props;
 
 		return (
 			<li>
-				<Link to={`/category/${post.category}/${post.id}`}><h4>{post.title}</h4></Link>
-				<p><b>Category:</b> {post.category}</p>
-				<p><b>Vote Score:</b> {post.voteScore}</p>
-				<p><b>Date:</b> {this.convertTimestampToDate(post.timestamp)}</p>
-				<p><b>Comment Count:</b> {comments.length}</p>
+				<div className="row">
+					<div className="col-sm-12">
+						<Link to={`/category/${post.category}/${post.id}`}><h4>{post.title}</h4></Link>
+						<p><b>Category:</b> {post.category}</p>
+						<p><b>Vote Score:</b> {post.voteScore}</p>
+						<p><b>Date:</b> {this.convertTimestampToDate(post.timestamp)}</p>
+						<p><b>Comment Count:</b> {comments.length}</p>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-sm-3">
+						<button className="btn btn-primary" onClick={() => this.onVoteClick("upVote")}>Up Vote</button>
+					</div>
+					<div className="col-sm-3">
+						<button className="btn btn-warning" onClick={() => this.onVoteClick("downVote")}>Down Vote</button>
+					</div>
+					<div className="col-sm-3">
+						<button className="btn btn-danger" onClick={() => this.props.deletePost(post.id)}>Delete</button>
+					</div>
+					<div className="col-sm-3">
+						<Link to={`/post/edit/${post.id}`} className="btn btn-success">Edit</Link>
+					</div>
+				</div>
 			</li>
 		);
 	}
@@ -30,4 +56,4 @@ function mapStateToProps(state, props) {
 	}
 }
 
-export default connect(mapStateToProps, null)(PostSummary);
+export default connect(mapStateToProps, {postVote, deletePost})(PostSummary);
